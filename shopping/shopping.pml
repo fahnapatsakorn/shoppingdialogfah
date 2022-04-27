2977883e38243c1f338f41d4fd8a5743dfba23f0
fahnapatsakorn <103438631+fahnapatsakorn@users.noreply.github.com>
2022-04-27 16:39:43 +0700

    Add files via upload

---
+++ shopping/shopping.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="shopping" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs>
+        <Dialog name="my_dialog" src="my_dialog/my_dialog.dlg" />
+    </Dialogs>
+    <Resources />
+    <Topics>
+        <Topic name="my_dialog_enu" src="my_dialog/my_dialog_enu.top" topicName="my_dialog" language="en_US" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ shopping/translations/translation_en_US.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I remember you!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I remember you!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/_Face Rec/_Say to recognize face</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Begin to recognize face</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Begin to recognize face</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/detectface</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I am detecting your face</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am detecting your face</translation>
+        </message>
+    </context>
+</TS>
